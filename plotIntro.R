9a0c28e544137fbc6831a58588c48ae5db75a9ef
Duaa Ahmed <duaa@tywls-astoria.org>
2023-07-12 01:52:21 -0400

    Lab: intro to plots

---
+++ plotIntro.R
@@ -0,0 +1,12 @@
+install.packages("ggplot2")
+library(ggplot2)
+ggplot(data=dataa)
+str(dataa)
+##ggplot(data = dataa, aes(x = User.Rating, y = Reviews))+
+##geom_point()
+##ggplot(data = dataa, aes(x = User.Rating))+
+  ##geom_histogram(bins = 30)
+
+##ggplot(data = dataa, aes(x = Genre))+
+##geom_bar()
+
